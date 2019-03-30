e1da9484a0818cc7cc18feb8344be2ab3cadb462
Fire <pascaldebruijngsm@gmail.com>
2019-03-30 15:04:20 +0100

    Auto stash before merge of "master" and "origin/master"

--- Auto markt/src/AutomarktApp.java
+++ Auto markt/src/AutomarktApp.java
@@ -1,10 +1,35 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class AutomarktApp extends Application {
     @Override
     public void start(Stage stage) {
         stage.setTitle("Advertentie Markt");
+        Label namelabel = new Label("Name: ");
+        TextField nametext = new TextField();
+        Label personIdLabel = new Label("Password: ");
+        TextField personIdText = new TextField();
+
+
+        Button button = new Button("ADD");
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        stage.getScene();
         stage.show();
     }
 
